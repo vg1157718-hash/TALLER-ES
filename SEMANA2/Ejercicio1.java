e479b89c3b4b9371d48342e85fb8a693f98da951
vg1157718-hash <vg1157718@gmail.com>
2026-02-10 16:32:14 +0000

    Primer ejercicio con arreglo

---
+++ SEMANA2/Ejercicio1.java
@@ -0,0 +1,13 @@
+package SEMANA2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //Formal -> Definido directamente los valores arreglo
+        int[] a = {4, 8, 9, 6, 1, 2};
+        // iterrar los elementos del arreglo 
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a["+i+"]="+a[i]);
+        }
+    }
+
+}
